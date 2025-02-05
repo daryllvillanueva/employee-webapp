b85b8b5060555293a5e6e03e784fc46e304d7c2d
Daryll <daryll.02javier@gmail.com>
2025-02-06 01:18:09 +0800

    Add Fonts, UI Components, Pages and DarkMode

--- ReactEmployeeWebApp/src/App.jsx
+++ ReactEmployeeWebApp/src/App.jsx
@@ -1,12 +1,31 @@
-import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./components/Home";
+import PageNotFound from "./components/partials/PageNotFound";
+import Employees from "./components/Employees";
+import React, { useEffect, useState } from 'react'
+import Header from "./components/partials/Header";
 
 function App() {
+  const [isToggled, setIsToggled] = useState(false);
+  const handleToggled = () => {
+    setIsToggled(!isToggled);
+  };
+
+  useEffect(() => {
+    if (isToggled) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [isToggled]);
+
   return (
     <Router>
+      <Header handleToggled={handleToggled} isToggled={isToggled} />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home isToggled={isToggled} handleToggled={handleToggled}/>}/>
+        <Route path="/employees" element={<Employees isToggled={isToggled} handleToggled={handleToggled}/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
       </Routes>
     </Router>
   )
